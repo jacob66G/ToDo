91e4581863ddfaf932ce7587041eb1fc8448f21f
JAKUB GERWEL <kuba.gerwel@gmail.com>
2025-04-16 12:55:26 +0200

    dodanie warstwy  dao

---
+++ backend/src/main/java/com/example/ToDo/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.ToDo.repositories;
+
+import com.example.ToDo.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByUserId(Long userId);
+}
---
+++ backend/src/main/java/com/example/ToDo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.ToDo.repositories;
+
+import com.example.ToDo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
