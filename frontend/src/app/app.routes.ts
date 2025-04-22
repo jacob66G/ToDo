bb89f596156d1d8649ee8ccb518a827bbeecf7bf
Mateusz Prokopiuk <mateuszpro@op.pl>
2025-04-22 19:52:35 +0200

    category display

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'home-page', component: HomePageComponent },
+    { path: 'user-page', component:  UserPageComponent},
 ];
---
+++ frontend/src/app/pages/user-page/user-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CategoryManageComponent } from '../../components/category-manage/category-manage.component';
+
+@Component({
+  selector: 'app-user-page',
+  imports: [CategoryManageComponent],
+  templateUrl: './user-page.component.html',
+  styleUrl: './user-page.component.css'
+})
+export class UserPageComponent {
+
+}
