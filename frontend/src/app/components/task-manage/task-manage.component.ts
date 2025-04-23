ddc63b8b6788ef1bc6dd080dfe1896ed663de40f
Mateusz Prokopiuk <mateuszpro@op.pl>
2025-04-23 10:41:17 +0200

    manage task component created

---
+++ frontend/src/app/components/task-manage/task-manage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-manage',
+  imports: [],
+  templateUrl: './task-manage.component.html',
+  styleUrl: './task-manage.component.css',
+  standalone: true,
+})
+export class TaskManageComponent {
+
+}
--- frontend/src/app/pages/user-page/user-page.component.ts
+++ frontend/src/app/pages/user-page/user-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CategoryManageComponent } from '../../components/category-manage/category-manage.component';
+import { TaskManageComponent } from '../../components/task-manage/task-manage.component';
 
 @Component({
   selector: 'app-user-page',
-  imports: [CategoryManageComponent],
+  imports: [CategoryManageComponent, TaskManageComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.css'
 })
