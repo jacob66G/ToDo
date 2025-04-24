4689679193c3f31723376d7cd10e470ee082ee00
Mateusz Prokopiuk <mateuszpro@op.pl>
2025-04-24 08:46:54 +0200

    Navbar added

--- frontend/src/app/pages/user-page/user-page.component.ts
+++ frontend/src/app/pages/user-page/user-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CategoryManageComponent } from '../../components/category-manage/category-manage.component';
 import { TaskManageComponent } from '../../components/task-manage/task-manage.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-user-page',
-  imports: [CategoryManageComponent, TaskManageComponent],
+  imports: [CategoryManageComponent, TaskManageComponent, NavbarComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.css'
 })
